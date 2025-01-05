056e6cd90ef92ffdf4ed8915a2a3e068428368ca
Ken <yash20004x17@gmail.com>
2025-01-05 10:09:35 +0530

    Update in existing files

--- prsnl-projects/script.js
+++ prsnl-projects/script.js
@@ -1,7 +1,57 @@
-let num = 22
-let newNum = new Number(22)
-let newNum2 = Number("abc")
+// const parent = document.querySelector('.parent')
+// console.log(parent);
+// console.log(parent.children);
+// // console.log(parent.children[1].innerHTML);
 
-console.log(typeof num);
-console.log(typeof newNum);
-console.log(typeof newNum2);
+// for (let i = 0; i < parent.children.length; i++) {
+//     const element = parent.children[i].innerHTML;
+//     console.log(element);
+//     parent.children[i].style.color="blue";
+    
+// }
+// const div = document.createElement('div');
+// div.className= "main";
+// div.id = Math.round(Math.random() * 10 + 1);
+// div.setAttribute("title", "Generated Title");
+// div.style.backgroundColor = "orange";
+// div.style.padding = "10px";
+// // div.innertext="Yo";
+// const addText = document.createTextNode("Yo")
+// div.appendChild(addText)
+// document.body.appendChild(div)
+// console.log(div);
+
+
+// const li = document.createElement('li')
+// const ul = document.querySelector('ul')
+// const add = document.getElementById('create')
+
+// function addLi(){
+//     li.innerText="Rust"
+//     document.querySelector('.language').appendChild(li)
+// }
+// add.addEventListener('click',function (item) {
+//     addLi(item)
+// })
+
+// function addLanguage(language){
+//     const li = document.createElement('li');
+//     li.innerHTML = `${language}`
+//     document.querySelector('.language').appendChild(li)
+// }
+
+// const add = document.getElementById('create');
+// const inputText = document.getElementById('addText')
+
+
+// add.addEventListener('click',function (){
+//     console.log(inputText.value);
+//     addLanguage(inputText.value)
+// })
+
+const user = {
+    username: "Yash Yeager",
+    loginCount: 10,
+    isLoggedIn: true
+}
+console.log(user["username"]);
